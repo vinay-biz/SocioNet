a8d3b938966e995494e3131b5faae03da882cf2e
Vinay Pandey <vinaypandey112003@gmail.com>
2024-07-28 23:21:47 +0530

    Addes auth, post, users controller, added test data to mongoose, created authentication middleware, added routes and models, backend completed

--- server/index.js
+++ server/index.js
@@ -6,11 +6,21 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
+import authRoutes from "./routes/auth.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
-dotenv.config();
+
+
+dotenv.config(); 
 
 //Configurations
 
@@ -38,12 +48,20 @@ const upload = multer({ storage });
 
 //routes with files
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
+//routes
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 //Mongoose setup
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    
+    //User.insertMany(users);
+    //Post.insertMany(posts);
 })
 .catch((error) => console.log(`${error} did not connect`));
 
